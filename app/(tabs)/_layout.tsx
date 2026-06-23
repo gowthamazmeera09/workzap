28d74639c76b202a7907beb27c6fb49ad1120aa3
gowthamazmeera <gowthamazmeera@gmail.com>
2026-06-23 22:40:12 +0530

    added some images

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,60 @@
 import { Tabs } from "expo-router";
+import { Image } from "react-native";
 import React from "react";
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2962FF",
+        tabBarInactiveTintColor: "#777",
+        tabBarStyle: {
+          height: 65,
+          paddingBottom: 8,
+          paddingTop: 6,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../../assets/images/home.png")}
+              style={{
+                width: 26,
+                height: 26,
+                opacity: focused ? 1 : 0.6,
+              }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
 
-  <Tabs.Screen
-    name="index"
-    options={{
-      title: "Home",
-      headerShown: false
-    }}
-  />
-
-  <Tabs.Screen
-    name="worker"
-    options={{
-      title: "Worker",
-      headerShown: false
-    }}
-  />
-
-</Tabs>
+      <Tabs.Screen
+        name="worker"
+        options={{
+          title: "Worker",
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../../assets/images/worker.png")}
+              style={{
+                width: 26,
+                height: 26,
+                opacity: focused ? 1 : 0.6,
+              }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
