0dcdfbd8fb1c8035386386157f9b1c9200b057c7
gowthamazmeera <gowthamazmeera@gmail.com>
2026-07-12 00:22:40 +0530

    added localhost

--- constants/api.ts
+++ constants/api.ts
@@ -1,3 +1,5 @@
-const API_URL = "https://workzap-backend.onrender.com";
+// const API_URL = "https://workzap-backend.onrender.com";
+
+const API_URL = "http://192.168.1.5:5000";
 
 export default API_URL;
